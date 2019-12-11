127b6e15636b3abdac6ade68876cc2422cfd7107
David B <d.jbradley.ii@gmail.com>
2019-12-11 13:26:27 -0800

    adds test for api/users

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,17 @@
 const app = require('../src/app')
 
-describe('App', () => {
+describe.skip('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
       .get('/')
       .expect(200, 'Hello, world!')
+  })
+})
+
+describe('App', () => {
+  it('GET /api/users responds with 200', () => {
+    return supertest(app)
+      .get('/api/users')
+      .expect(200)
   })
 })
