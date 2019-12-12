e40c912634977e83502477390c47dbcaeeebe20f
David B <d.jbradley.ii@gmail.com>
2019-12-11 17:32:04 -0800

    adds validation middleware for user authentication

--- src/app.js
+++ src/app.js
@@ -21,6 +21,21 @@ app.use(
   })
 );
 
+/* 
+
+REQUEST VALIDATION
+
+app.use(function validateBearerToken(req, res, next){
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+      return res.status(401).json({ error: 'Unauthorized request' })
+  }
+    // move to the next middleware
+  next();
+}); */
+
 app.get('/api/*', (req, res) => {
     res.json({ok: true});
 });
