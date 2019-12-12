246f623d26dca0adfb080252ed2935751632ca3d
David B <d.jbradley.ii@gmail.com>
2019-12-11 22:32:05 -0800

    adds TEST_DB_URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
-    DB_URL: process.env.DB_URL || 'postgresql://dbradley:password@localhost/social-dining-app-test'
+    DB_URL: process.env.DB_URL || 'postgresql://dbradley:password@localhost/social-dining-app',
+    TEST_DB_URL:'postgresql://dbradley:password@localhost/social-dining-app-test'
   }
