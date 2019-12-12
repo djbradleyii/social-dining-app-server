4b711d2a7023758b2e6623ce405161be61f7b45c
David B <d.jbradley.ii@gmail.com>
2019-12-11 17:31:15 -0800

    adds tests to test get requests to users endpoint

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,4 +1,5 @@
 const app = require('../src/app')
+const users = require('../src/users.js');
 
 describe.skip('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
@@ -13,5 +14,15 @@ describe('App', () => {
     return supertest(app)
       .get('/api/users')
       .expect(200)
+  });
+  
+  it('GET /api/users responds with 200 and array of all users', () => {
+    return supertest(app)
+      .get('/api/users')
+      .expect(200)
+      .then(res =>{
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+      })
   })
 })
