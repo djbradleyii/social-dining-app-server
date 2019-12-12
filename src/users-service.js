d1a78e23e44bdf316d8e6943506071b3d166eba4
David B <d.jbradley.ii@gmail.com>
2019-12-12 00:02:00 -0800

    adds insertUser with logic

--- src/users-service.js
+++ src/users-service.js
@@ -3,6 +3,15 @@ const UsersService = {
         return knex.select('*')
             .from('users')
     },
+    insertUser(knex, newUser){
+        return knex
+            .insert(newUser)
+            .into('users')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
 };
 
 module.exports = UsersService;
