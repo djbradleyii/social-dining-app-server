2b6e62b1f12a5ae821ad5354a7b99d21d0046d2f
David B <d.jbradley.ii@gmail.com>
2019-12-11 20:28:21 -0800

    changes double quotes to single quotes

--- seeds/seed.users.sql
+++ seeds/seed.users.sql
@@ -1,16 +1,16 @@
 INSERT INTO users (fname, lname, dob, email, password, marital_status, occupation, gender, bio)
 VALUES
-    ('Rick', 'Mcqueeney','03/02/1979',"rmcqueeney@gmail.com", "password1","Married","Marketing", "Male",
+    ('Rick', 'Mcqueeney','03/02/1979','rmcqueeney@gmail.com', 'password1','Married','Marketing', 'Male',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
-    ('Reynalda', 'Goodman','11/14/1980',"rgoodman@yahoo.com", "password2","Single","Lawyer", "Female",
+    ('Reynalda', 'Goodman','11/14/1980','rgoodman@yahoo.com', 'password2','Single','Lawyer', 'Female',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
-    ('Casey', 'Pilkenton','04/24/1993',"cpilkenton@aol.com", "password3","Separated","Engineer", "Male",
+    ('Casey', 'Pilkenton','04/24/1993','cpilkenton@aol.com', 'password3','Separated','Engineer', 'Male',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
-    ('Venetta', 'Dossey','05/02/1965',"rmcqueeney@gmail.com", "password4","Single","Artist", "Female",
+    ('Venetta', 'Dossey','05/02/1965','rmcqueeney@gmail.com', 'password4','Single','Artist', 'Female',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
-    ('David', 'Bell','10/31/1983',"dbell@gmsn.com", "password5","Single","Full Stack Developer", "Male",
+    ('David', 'Bell','10/31/1983','dbell@gmsn.com', 'password5','Single','Full Stack Developer', 'Male',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
-    ('Dewayne', 'Piper','02/24/1990',"rmcqueeney@yahoo.com", "password6","Married","Chef", "Male",
+    ('Dewayne', 'Piper','02/24/1990','rmcqueeney@yahoo.com', 'password6','Married','Chef', 'Male',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
-    ('Rebecca', 'Rose','02/01/1990',"rrose@aol.com", "password7","Single","Entrepreneur", "Female",
+    ('Rebecca', 'Rose','02/01/1990','rrose@aol.com', 'password7','Single','Entrepreneur', 'Female',
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?');
