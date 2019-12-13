18b9605865c61e40b09edbaca45d345e730a03b9
David B <d.jbradley.ii@gmail.com>
2019-12-12 17:39:18 -0800

    updates the time format; adds space before the meridies designations

--- src/events.js
+++ src/events.js
@@ -7,7 +7,7 @@ module.exports = [
 		restaurant : "Water Grill",
 		address : "123 2nd St, Los Angeles, CA 90001",
 		date : "01/14/2020",
-		time : "05:30PM",
+		time : "05:30 PM",
 		description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed leo non lacus commodo semper. Nam finibus quam ut urna porttitor tempor eu sed metus.",
 		singlesOnly : "No",
 		attendees : 4
@@ -20,7 +20,7 @@ module.exports = [
 		restaurant : "Sonora Town",
 		address : "321 4th St, Los Angeles, CA 90003",
 		date : "02/14/2020",
-		time : "07:00PM",
+		time : "07:00 PM",
 		description : "Praesent ut tellus quis quam accumsan gravida id viverra leo. Phasellus congue malesuada arcu, blandit malesuada augue mollis id. Nulla dictum lectus quis massa pulvinar luctus. Ut vel lobortis justo, id sagittis lacus. Vestibulum ut interdum dolor. Suspendisse bibendum condimentum mi at faucibus. Morbi eu mauris accumsan, rhoncus dui vitae, blandit leo.",
 		singlesOnly : "Yes",
 		attendees : 10
@@ -33,7 +33,7 @@ module.exports = [
 		restaurant : "Daikokuya",
 		address : "30923 Union Ave, Los Angeles, CA 90301",
 		date : "03/02/2020",
-		time : "07:45PM",
+		time : "07:45 PM",
 		description : "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque blandit eros in enim efficitur pharetra. Sed sit amet tellus sed libero malesuada tempor nec in ligula. Morbi hendrerit metus justo, id mattis sapien semper sed. Duis blandit tempus semper. Morbi vel metus tortor. Pellentesque ultrices lobortis ante.",
 		singlesOnly : "No",
 		attendees : 6
@@ -46,7 +46,7 @@ module.exports = [
 		restaurant : "Button Mash",
 		address : "1391 Sunset Blvd., Los Angeles, CA 90026",
 		date : "01/14/2020",
-		time : "06:30PM",
+		time : "06:30 PM",
 		description : "In tincidunt a risus ut mattis. Sed justo eros, laoreet eu risus id, luctus auctor ipsum. Aenean vestibulum tellus vitae est viverra mattis. Phasellus id hendrerit risus, sit amet lobortis tellus. Proin tristique et mauris a imperdiet.",
 		singlesOnly : "No",
 		attendees : 25
@@ -59,7 +59,7 @@ module.exports = [
 		restaurant : "Scum and Villainy Cantina",
 		address : "6377 Hollywood Blvd., Los Angeles, CA 90028",
 		date : "05/04/2020",
-		time : "06:45PM",
+		time : "06:45 PM",
 		description : "In felis mi, efficitur at porttitor ac, egestas quis quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed eu nunc nec lorem malesuada ornare. Maecenas malesuada pellentesque risus, et egestas eros euismod eu.",
 		singlesOnly : "No",
 		attendees : 20
@@ -72,7 +72,7 @@ module.exports = [
 		restaurant : "Republique",
 		address : "624 S La Brea Ave Hancock Park",
 		date : "07/04/2020",
-		time : "06:45PM",
+		time : "06:45 PM",
 		description : "French eatery with some of the best, fresh-baked pastries in Los Angeles! Join us for some delicious food, ice breakers and much more.",
 		singlesOnly : "No",
 		attendees : 13
@@ -85,7 +85,7 @@ module.exports = [
 		restaurant : "Running Goose",
 		address : "1620 N Cahuenga Blvd Los Angeles, CA 90028",
 		date : "10/04/2020",
-		time : "06:45PM",
+		time : "06:45 PM",
 		description : "Inspired by the widely undiscovered specialties of Central America and the Yucatan, the Goose is a celebration of southern California's hidden pockets of cultural influence.",
 		singlesOnly : "No",
 		attendees : 10
@@ -98,7 +98,7 @@ module.exports = [
 		restaurant : "JOEY DTLA",
 		address : "700 W 7th St, Los Angeles, CA 90017",
 		date : "02/04/2020",
-		time : "06:45PM",
+		time : "06:45 PM",
 		description : "Genuine Warmth & Care are always on the table",
 		singlesOnly : "No",
 		attendees : 6
