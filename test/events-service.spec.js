762e7f70e2d2ba86b966f4602638635ef269de6a
David B <d.jbradley.ii@gmail.com>
2019-12-12 16:30:47 -0800

    create events-service spec from users-service spec

---
+++ test/events-service.spec.js
@@ -0,0 +1,206 @@
+const knex = require('knex');
+const EventsService = require('../src/events-service');
+
+describe(`Events service object`, function() {
+    let db;
+
+    let testUsers = [
+        {
+            id: 1,
+            fname : "Rick",
+            lname : "Mcqueeney",
+            dob : new Date('10/31/1983'),
+            email : "rmcqueeney@gmail.com",
+            password : "password1",
+            marital_status : "Married",
+            occupation : "Marketing",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: new Date('2019-12-11T16:28:32.615Z')
+        },
+        {
+            id: 2,
+            fname : "Summer",
+            lname : "Lane",
+            dob : new Date('02/02/1972'),
+            email : "slane@gmail.com",
+            password : "password2",
+            marital_status : "Married",
+            occupation : "Fashion Designer",
+            gender : "Female",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: new Date('2019-12-11T16:28:32.615Z')
+        },
+        {
+            id: 3,
+            fname : "Larry",
+            lname : "Savage",
+            dob : new Date('06/10/1975'),
+            email : "lsavage@aol.com",
+            password : "password3",
+            marital_status : "Widow",
+            occupation : "Construction",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: new Date('2019-12-11T16:28:32.615Z')
+        },
+    ];
+
+    let testEvents = [
+        {
+            id: 1,
+            fname : "Rick",
+            lname : "Mcqueeney",
+            dob : new Date('10/31/1983'),
+            email : "rmcqueeney@gmail.com",
+            password : "password1",
+            marital_status : "Married",
+            occupation : "Marketing",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: new Date('2019-12-11T16:28:32.615Z')
+        },
+        {
+            id: 2,
+            fname : "Summer",
+            lname : "Lane",
+            dob : new Date('02/02/1972'),
+            email : "slane@gmail.com",
+            password : "password2",
+            marital_status : "Married",
+            occupation : "Fashion Designer",
+            gender : "Female",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: new Date('2019-12-11T16:28:32.615Z')
+        },
+        {
+            id: 3,
+            fname : "Larry",
+            lname : "Savage",
+            dob : new Date('06/10/1975'),
+            email : "lsavage@aol.com",
+            password : "password3",
+            marital_status : "Widow",
+            occupation : "Construction",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: new Date('2019-12-11T16:28:32.615Z')
+        },
+    ];
+
+    before(() => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL,
+        })
+    });
+
+    before(() => db('users').truncate());
+
+    afterEach(() => db('users').truncate());
+
+    after(() => db.destroy());
+
+    context(`Given 'users' has data`, () => {
+        beforeEach(() => {
+            return db
+                .into('users')
+                .insert(testUsers)
+        });
+
+        it(`getAllUsers() resolves all users from 'users' table`, () => {
+            return UsersService.getAllUsers(db)
+                .then(actual => {
+                    expect(actual).to.eql(testUsers.map(user => ({
+                               ...user,
+                               dob: new Date(user.dob),
+                               date_created: new Date(user.date_created)
+                    })))
+                })
+        })
+
+        it(`getUserByID() resolves a user by id from 'user' table`, () => {
+            const thirdId = 3;
+            const thirdTestUser = testUsers[thirdId - 1];
+            return UsersService.getUserById(db, thirdId)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: thirdId,
+                        fname: thirdTestUser.fname,
+                        lname: thirdTestUser.lname,
+                        dob: new Date(thirdTestUser.dob),
+                        email: thirdTestUser.email,
+                        password: thirdTestUser.password,
+                        marital_status: thirdTestUser.marital_status,
+                        occupation: thirdTestUser.occupation,
+                        gender: thirdTestUser.gender,
+                        bio: thirdTestUser.bio,
+                        date_created: new Date(thirdTestUser.date_created)
+                    })
+                })
+        })
+
+        it(`deleteUser() removes a user by id from 'user' table`, () => {
+            const userId = 3;
+            return UsersService.deleteUser(db, userId)
+                .then(() => UsersService.getAllUsers(db))
+                .then(allUsers => {
+                    const expected = testUsers.filter(user => user.id !== userId);
+                    expect(allUsers).to.eql(expected)
+                })
+        })
+
+        it(`updateUserInfo() updates a users information from 'users' table`, () => {
+            const idOfUserToUpdate = 3;
+            const newUserInfo = {
+                bio: "new bio coming soon"
+            };
+            return UsersService.updateUserInfo(db, idOfUserToUpdate, newUserInfo)
+            .then(() => UsersService.getUserById(db, idOfUserToUpdate))
+            .then(user => {
+                expect(user).to.eql({
+                    id: idOfUserToUpdate,
+                    ...user,
+                    bio: newUserInfo.bio
+                })
+            })
+        })
+    })
+
+    context(`Given 'users' table has no data`, () => {
+        beforeEach(() => db('users').truncate());
+
+        it(`insertUser() inserts a new user and resolves the new user with an 'id'`, () => {
+            const newUser = {
+                fname : "Samantha",
+                lname : "Lake",
+                dob : new Date('05/22/1980'),
+                email : "slake@lakescakes.com",
+                password : "lakescakes1",
+                marital_status : "Divorced",
+                occupation : "Baker",
+                gender : "Female",
+                bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+                date_created: new Date('2019-12-11T16:28:32.615Z')
+            }
+
+            return UsersService.insertUser(db, newUser)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: 1,
+                        fname: newUser.fname,
+                        lname: newUser.lname,
+                        dob: new Date(newUser.dob),
+                        email: newUser.email,
+                        password: newUser.password,
+                        marital_status: newUser.marital_status,
+                        occupation: newUser.occupation,
+                        gender: newUser.gender,
+                        bio: newUser.bio,
+                        date_created: new Date(newUser.date_created)
+                    })
+                })
+        })
+
+    })
+})
