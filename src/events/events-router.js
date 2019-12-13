a872a615def5f5d20143755dec19ffdf85c22fc3
David B <d.jbradley.ii@gmail.com>
2019-12-13 01:45:19 -0800

    adds logic for GET method for /api/events endpoint

--- src/events/events-router.js
+++ src/events/events-router.js
@@ -6,7 +6,24 @@ const bodyParser = express.json()
 eventsRouter
   .route('/api/events')
   .get((req, res) => {
-    // move implementation logic into here
+    const knexInstance = req.app.get('db')
+    EventsService.getAllEvents(knexInstance)
+    .then(events => { 
+      res.json(events.map(event => ({
+        id: event.id,
+        organizer: parseInt(event.organizer),
+        title: event.title,
+        event_purpose: event.event_purpose,
+        restaurant: event.restaurant,
+        address: event.address,
+        date: new Date(event.date),
+        time: event.time,
+        description: event.description,
+        singles_only: event.singles_only,
+        date_created: new Date(event.date_created)
+        })))
+    })
+    .catch(next)
   })
   .post(bodyParser, (req, res) => {
     // move implementation logic into here
