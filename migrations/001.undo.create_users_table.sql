d200cead6d3e2249f5f800f17112bba22bc7149c
David B <d.jbradley.ii@gmail.com>
2019-12-13 01:09:12 -0800

    adds cascade to resolve ENUM dependency conflict

--- migrations/001.undo.create_users_table.sql
+++ migrations/001.undo.create_users_table.sql
@@ -1,4 +1,4 @@
-DROP TYPE IF EXISTS marital_choices;
-DROP TYPE IF EXISTS gender_choices;
+DROP TYPE IF EXISTS marital_choices CASCADE;
+DROP TYPE IF EXISTS gender_choices CASCADE;
 
 DROP TABLE IF EXISTS users;
