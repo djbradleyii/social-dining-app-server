3a664b7f235cefb8cf381a4c8a006a341a2149d6
David B <d.jbradley.ii@gmail.com>
2019-12-12 16:18:44 -0800

    adds seed.attendees.sql to seed the attendees table for testing

---
+++ seeds/seed.attendees.sql
@@ -0,0 +1,10 @@
+INSERT INTO attendees (user_id, event_id)
+VALUES
+    (2, "Bring your Business Cards", "Networking", "JOEY DTLA", "700 W 7th St, Los Angeles, CA 90037", "02/07/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE),
+    (1, "Happy Hour Fun", "Social", "Sonora Town", "100 W 7th St, Los Angeles, CA 90057", "06/24/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE),
+    (3, "Single Mingle", "Singles Night", "Water Grill", "200 W 7th St, Los Angeles, CA 90037", "07/03/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE),
+    (4, "Social Ramen", "Social", "Daikokuya", "300 W 7th St, Los Angeles, CA 90027", "02/22/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE),
+    (5, "Board Game Bananza", "Game Night", "Button Mash", "400 W 7th St, Los Angeles, CA 95017", "05/04/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE),
+    (6, "French Cuisine with Wine", "Social", "Scum and Villainy Cantina", "500 W 7th St, Los Angeles, CA 93017", "09/04/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE),
+    (2, "Tapas Anyone?", "Networking", "Running Goose", "600 W 7th St, Los Angeles, CA 90517", "11/04/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE),
+    (1, "Central America Transport", "Networking", "Republique", "900 W 7th St, Los Angeles, CA 98017", "12/04/2020", "06:45PM", "Genuine Warmth & Care are always on the table", FALSE);
