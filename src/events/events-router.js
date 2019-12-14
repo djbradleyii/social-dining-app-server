3832ca7f58eeb34b2d2a6925b35dd8d94d1482ec
David B <d.jbradley.ii@gmail.com>
2019-12-13 23:35:01 -0800

    Serializes user input and updates error code messages for events router

--- src/events/events-router.js
+++ src/events/events-router.js
@@ -5,25 +5,28 @@ const bodyParser = express.json();
 const logger = require('../logger');
 const xss = require('xss');
 
+const serializeEvent = event => ({
+  id: parseInt(event.id),
+  organizer: parseInt(event.organizer),
+  title: xss(event.title),
+  event_purpose: xss(event.event_purpose),
+  restaurant: xss(event.restaurant),
+  address: xss(event.address),
+  date: new Date(event.date),
+  time: event.time,
+  description: xss(event.description),
+  singles_only: event.singles_only,
+  date_created: new Date(event.date_created)
+  })
+
+
 eventsRouter
   .route('/api/events')
   .get((req, res, next) => {
     const knexInstance = req.app.get('db')
     EventsService.getAllEvents(knexInstance)
     .then(events => { 
-      res.json(events.map(event => ({
-        id: event.id,
-        organizer: parseInt(event.organizer),
-        title: event.title,
-        event_purpose: event.event_purpose,
-        restaurant: event.restaurant,
-        address: event.address,
-        date: new Date(event.date),
-        time: event.time,
-        description: event.description,
-        singles_only: event.singles_only,
-        date_created: new Date(event.date_created)
-        })))
+      res.json(events.map(serializeEvent))
     })
     .catch(next)
   })
@@ -63,24 +66,12 @@ eventsRouter
 eventsRouter
   .route('/api/events/:event_id')
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db');
-    const { event_id } = req.params;
-  
-    eventsService.getEventsById(knexInstance, event_id)
-    .then(event => { 
-      if(!event){
-        return res.status(404).json({
-          error: { message: `Event doesn't exist` }
-        })
-      }
-      res.json(event)
-    })
-    .catch(next)
+    res.json(serializeEvent(res.event));
   })
-  .patch((req, res, next) => {
-    const { event_id } = parseInt(req.params); //parseInt() for integers
-    const {title, event_purpose, time, description} = req.body;
-    const requiredFields = { title, event_purpose, time, description };
+  .patch(bodyParser, (req, res, next) => {
+    const { event_id } = req.params; //parseInt() for integers
+    const {organizer, title, event_purpose, restaurant, address, date, time, description, singles_only} = req.body;
+    const requiredFields = { organizer, title, event_purpose, restaurant, address, date, time, description, singles_only };
   
     for (const [key, value] of Object.entries(requiredFields)) {
       if (value == null) {
@@ -90,26 +81,23 @@ eventsRouter
       }
     } 
   
-    const updates = {title, event_purpose, time, description};
-    EventService.updateEventById(req.app.get('db'), event_id, updates)
+    const updates = {organizer, title, event_purpose, restaurant, address, date, time, description, singles_only};
+    EventsService.updateEventById(req.app.get('db'), event_id, updates)
     .then(() => {
       res.status(204).end();
     })
     .catch(next);
   })
-  .delete((req, res) => {
-    const { event_id } = parseInt(req.params); //parseInt() for integers
-
-    if(!event_id){
+  .delete((req, res, next) => {
+    if(!req.params.event_id){
       logger.error(`Event id is required.`);
       return res.status(400).send('Invalid data')
     }
-  
-    EventsService.deleteEvent(res.app.get('db'), event_id)
+    EventsService.deleteEvent(res.app.get('db'), req.params.event_id)
       .then( (count) => {
         if(count === 0){
           return res.status(404).json({
-            error: { message: `Bookmark does not exist`}
+            error: { message: `Event does not exist`}
           })
         }
         res
@@ -117,7 +105,7 @@ eventsRouter
         .end();
       })
       .catch(next)
-      logger.info(`Event with event_id ${event_id} deleted.`);  
+      logger.info(`Event with event_id ${req.params.event_id} deleted.`);  
   })
 
 module.exports = eventsRouter
