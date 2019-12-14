0460246f7903e92a0c1b8b083d8ad83ac51e90d1
David B <d.jbradley.ii@gmail.com>
2019-12-14 14:25:27 -0800

    updates the dob and date_created for users fixtures for testing

--- test/users.fixtures.js
+++ test/users.fixtures.js
@@ -3,38 +3,38 @@ function makeUsersArray(){
         {
             fname : "Rick",
             lname : "Mcqueeney",
-            dob : '1919-12-22T16:28:32.615Z',
+            dob : '10/10/1980',
             email : "rmcqueeney@gmail.com",
             password : "password1",
             marital_status : "Married",
             occupation : "Marketing",
             gender : "Male",
             bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: '1919-12-22T16:28:32.615Z'
+            date_created: '10/10/1980'
         },
         {
             fname : "Summer",
             lname : "Lane",
-            dob : '1919-12-22T16:28:32.615Z',
+            dob : '10/10/1980',
             email : "slane@gmail.com",
             password : "password2",
             marital_status : "Married",
             occupation : "Fashion Designer",
             gender : "Female",
             bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: '1919-12-22T16:28:32.615Z'
+            date_created: '10/10/1980'
         },
         {
             fname : "Larry",
             lname : "Savage",
-            dob : '1919-12-22T16:28:32.615Z',
+            dob : '10/10/1980',
             email : "lsavage@aol.com",
             password : "password3",
             marital_status : "Widow",
             occupation : "Construction",
             gender : "Male",
             bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: '1919-12-22T16:28:32.615Z'
+            date_created: '10/10/1980'
         },
     ];
 }
