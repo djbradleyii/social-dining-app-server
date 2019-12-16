0b72124b9e47c6bfb0d8ee955a1602d8f1dac85e
David B <d.jbradley.ii@gmail.com>
2019-12-15 22:59:49 -0800

    Updates all seeds for testing the application

--- seeds/seed.events.sql
+++ seeds/seed.events.sql
@@ -1,8 +1,8 @@
-INSERT INTO events (organizer, title, purpose, restaurant, address, date, time, description, singles_only)
+INSERT INTO events (organizer, title, event_purpose, restaurant, address, date, time, description, singles_only)
 VALUES
     (2, 'Bring your Business Cards', 'Networking', 'JOEY DTLA', '700 W 7th St, Los Angeles, CA 90037', '02/07/2020', '06:45PM', 'Genuine Warmth & Care are always on the table', false),
     (1, 'Happy Hour Fun', 'Social', 'Sonora Town', '100 W 7th St, Los Angeles, CA 90057', '06/24/2020', '06:45PM', 'Genuine Warmth & Care are always on the table', false),
-    (3, 'Single Mingle', 'Singles Night', 'Water Grill', '200 W 7th St, Los Angeles, CA 90037', '07/03/2020', '06:45PM', 'Genuine Warmth & Care are always on the table', false),
+    (3, 'Single Mingle', 'Singles Night', 'Water Grill', '200 W 7th St, Los Angeles, CA 90037', '07/03/2020', '06:45PM', 'Genuine Warmth & Care are always on the table', true),
     (4, 'Social Ramen', 'Social', 'Daikokuya', '300 W 7th St, Los Angeles, CA 90027', '02/22/2020', '06:45PM', 'Genuine Warmth & Care are always on the table', false),
     (5, 'Board Game Bananza', 'Game Night', 'Button Mash', '400 W 7th St, Los Angeles, CA 95017', '05/04/2020', '06:45PM', 'Genuine Warmth & Care are always on the table', false),
     (6, 'French Cuisine with Wine', 'Social', 'Scum and Villainy Cantina', '500 W 7th St, Los Angeles, CA 93017', '09/04/2020', '06:45PM', 'Genuine Warmth & Care are always on the table', false),
