dced2b3999772f88a309b613cd8d9b1a88eb4673
David B <d.jbradley.ii@gmail.com>
2019-12-17 13:00:40 -0800

    Updates test users for testing

--- test/users.fixtures.js
+++ test/users.fixtures.js
@@ -7,48 +7,6 @@ function makeUsersArray(){
             lname : "Mcqueeney",
             dob : '10/10/1980',
             email : "rmcqueeney@gmail.com",
-            password : bcrypt.hashSync("Password1!", 1),
-            marital_status : "Married",
-            occupation : "Marketing",
-            gender : "Male",
-            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: '10/10/1980'
-        },
-        {
-            fname : "Summer",
-            lname : "Lane",
-            dob : '10/10/1980',
-            email : "slane@gmail.com",
-            password : bcrypt.hashSync("Password1!", 1),
-            marital_status : "Married",
-            occupation : "Fashion Designer",
-            gender : "Female",
-            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: '10/10/1980'
-        },
-        {
-            fname : "Larry",
-            lname : "Savage",
-            dob : '10/10/1980',
-            email : "lsavage@aol.com",
-            password : bcrypt.hashSync("Password1!", 1),
-            marital_status : "Widow",
-            occupation : "Construction",
-            gender : "Male",
-            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: '10/10/1980'
-        },
-    ];
-}
-
-function makeAuthUsersArray(){
-    return [
-        {
-            id: "1",
-            fname : "Rick",
-            lname : "Mcqueeney",
-            dob : '10/10/1980',
-            email : "rmcqueeney@gmail.com",
             password : "Password1!",
             marital_status : "Married",
             occupation : "Marketing",
@@ -57,7 +15,6 @@ function makeAuthUsersArray(){
             date_created: '10/10/1980'
         },
         {
-            id: "2",
             fname : "Summer",
             lname : "Lane",
             dob : '10/10/1980',
@@ -70,7 +27,50 @@ function makeAuthUsersArray(){
             date_created: '10/10/1980'
         },
         {
-            id: "3",
+            fname : "Larry",
+            lname : "Savage",
+            dob : '10/10/1980',
+            email : "lsavage@aol.com",
+            password : "Password3!",
+            marital_status : "Widow",
+            occupation : "Construction",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: '10/10/1980'
+        },
+    ];
+}
+
+function makeUsersArrayServices(){
+    return [
+        {
+            id: 1,
+            fname : "Rick",
+            lname : "Mcqueeney",
+            dob : '10/10/1980',
+            email : "rmcqueeney@gmail.com",
+            password : "Password1!",
+            marital_status : "Married",
+            occupation : "Marketing",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: '10/10/1980'
+        },
+        {
+            id: 2,
+            fname : "Summer",
+            lname : "Lane",
+            dob : '10/10/1980',
+            email : "slane@gmail.com",
+            password : "Password2!",
+            marital_status : "Married",
+            occupation : "Fashion Designer",
+            gender : "Female",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: '10/10/1980'
+        },
+        {
+            id: 3,
             fname : "Larry",
             lname : "Savage",
             dob : '10/10/1980',
@@ -97,6 +97,6 @@ function seedUsers(db, users) {
 
 module.exports = {
     makeUsersArray,
-    makeAuthUsersArray,
+    makeUsersArrayServices,
     seedUsers
 }
