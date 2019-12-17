6719b6eee01ff715b21120eed1a81596ba3f051a
David B <d.jbradley.ii@gmail.com>
2019-12-16 17:20:26 -0800

    Updates authRouter to send user_id after login

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -33,8 +33,10 @@ authRouter
 
             const sub = dbUser.email
             const payload = { user_id: dbUser.id }
+
             res.send({
                 authToken: AuthService.createJwt(sub, payload),
+                user_id: dbUser.id
             })
         })
     })
