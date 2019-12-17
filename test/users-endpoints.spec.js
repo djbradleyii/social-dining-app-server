31594c3dca3096c60861060a82b57b4d75eca727
David B <d.jbradley.ii@gmail.com>
2019-12-16 21:16:44 -0800

    Updates tests to add protected endpoint tests and sets authorization with jwt token

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -1,9 +1,13 @@
 const knex = require('knex')
 const app = require('../src/app')
-const { makeUsersArray } = require('./users.fixtures')
+const { makeUsersArray } = require('./users.fixtures');
+const bcrypt = require('bcryptjs');
+const AuthService = require('../src/auth/auth-service');
+const helpers = require('./test-helpers');
 
 describe.only('Users Endpoints', function() {
   let db
+  let testUsers = makeUsersArray();
 
   before('make knex instance', () => {
 
@@ -15,33 +19,74 @@ describe.only('Users Endpoints', function() {
 
   })
 
-  after('disconnect from db', () => db.destroy())
+  function makeAuthHeader(user) { 
+   const token = AuthService.createJwt(user.email, {user_id: user.id});
+   return `Bearer ${token}`
+  }
 
   before('clean the table', () => db.raw('TRUNCATE attendees, events, users RESTART IDENTITY CASCADE'))
 
+  beforeEach('insert users', () => {
+    return db
+      .into('users')
+      .insert(testUsers)
+  })
+
   afterEach('cleanup',() => db.raw('TRUNCATE attendees, events, users RESTART IDENTITY CASCADE'))
+
+  after('disconnect from db', () => db.destroy())
+    
+  
+    describe(`Protected endpoints`, () => {
+      beforeEach('insert articles', () => {
+        beforeEach('insert users', () => {
+          return db
+            .into('users')
+            .insert(testUsers)
+        })
+      })
+      describe(`GET /api/users/:user_id`, () => {
+        it(`responds 401 'Unauthorized request' when no credentials in token`, () => {
+          const userNoCreds = { user_name: '', password: '' };
+          return supertest(app)
+           .get(`/api/users/123`)
+           .set('Authorization', makeAuthHeader(userNoCreds))
+           .expect(401, { error: `Unauthorized request` })
+        })
+
+        it(`responds 401 'Unauthorized request' when invalid user`, () => {
+             const userInvalidCreds = { user_name: 'user-not', password: 'existy' }
+             return supertest(app)
+               .get(`/api/users/1`)
+               .set('Authorization', makeAuthHeader(userInvalidCreds))
+               .expect(401, { error: `Unauthorized request` })
+           })
+           
+        it(`responds 401 'Unauthorized request' when invalid password`, () => {
+          const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
+          return supertest(app)
+            .get(`/api/articles/1`)
+            .set('Authorization', makeAuthHeader(userInvalidPass))
+            .expect(401, { error: `Unauthorized request` })
+        })
+      })
+    })
 
   describe(`GET /api/users`, () => {
     context(`Given no users`, () => {
       it(`responds with 200 and an empty list`, () => {
         return supertest(app)
           .get('/api/users')
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(200, [])
       })
     })
 
     context('Given there are users in the database', () => {
-      const testUsers = makeUsersArray();
-
-      beforeEach('insert users', () => {
-        return db
-          .into('users')
-          .insert(testUsers)
-      })
-
       it('responds with 200 and all of the users', () => {
         return supertest(app)
           .get('/api/users')
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(200, testUsers)
       })
     })
@@ -53,24 +98,18 @@ describe.only('Users Endpoints', function() {
         const userId = 123456
         return supertest(app)
           .get(`/api/users/${userId}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(404, { error: { message: `User doesn't exist` } })
       })
     })
 
     context('Given there are users in the database', () => {
-      const testUsers = makeUsersArray();
-
-      beforeEach('insert users', () => {
-        return db
-          .into('users')
-          .insert(testUsers)
-      })
-
       it('responds with 200 and the specified user', () => {
         const userId = 2
         const expectedUser = testUsers[userId - 1]
         return supertest(app)
           .get(`/api/users/${userId}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(200, {
             ...expectedUser,
             dob: '10/10/1980',
@@ -102,6 +141,7 @@ describe.only('Users Endpoints', function() {
       }
       return supertest(app)
         .post('/api/users')
+        .set('Authorization', makeAuthHeader(testUsers[0]))
         .send(newUser)
         .expect(204)
     })
@@ -126,6 +166,7 @@ describe.only('Users Endpoints', function() {
 
         return supertest(app)
           .post('/api/users')
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .send(newUser)
           .expect(400, {
             error: { message: `Missing '${field}' in request body` }
@@ -140,24 +181,18 @@ describe.only('Users Endpoints', function() {
         const userId = 123456
         return supertest(app)
           .delete(`/api/users/${userId}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(404, { error: { message: `User doesn't exist` } })
       })
     })
 
     context('Given there are users in the database', () => {
-      const testUsers = makeUsersArray();
-
-      beforeEach('insert users', () => {
-        return db
-          .into('users')
-          .insert(testUsers)
-      })
-
       it('responds with 204 and removes the user', () => {
         const idToRemove = 2
         const expectedUsers = testUsers.filter(user => user.id !== idToRemove)
         return supertest(app)
           .delete(`/api/users/${idToRemove}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(204)
           .then(res =>
             supertest(app)
@@ -174,19 +209,12 @@ describe.only('Users Endpoints', function() {
         const userId = 123456
         return supertest(app)
           .delete(`/api/users/${userId}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(404, { error: { message: `User doesn't exist` } })
       })
     })
 
     context('Given there are users in the database', () => {
-      const testUsers = makeUsersArray();
-
-      beforeEach('insert users', () => {
-        return db
-          .into('users')
-          .insert(testUsers)
-      })
-
       it('responds with 204 and updates the user', () => {
         const idToUpdate = 2
         const updateUser = {
@@ -206,11 +234,13 @@ describe.only('Users Endpoints', function() {
         }
         return supertest(app)
           .patch(`/api/users/${idToUpdate}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .send(updateUser)
           .expect(204)
           .then(res =>
             supertest(app)
               .get(`/api/users/${idToUpdate}`)
+              .then()
               .expect(expectedUser)
           )
       })
@@ -219,6 +249,7 @@ describe.only('Users Endpoints', function() {
         const idToUpdate = 2
         return supertest(app)
           .patch(`/api/users/${idToUpdate}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .send({ irrelevantField: 'foo' })
           .expect(400, {
             error: { message: `Request body must contain either 'lname', 'dob', 'email', 'password', 'marital_status', 'bio', 'gender'`}
@@ -237,6 +268,7 @@ describe.only('Users Endpoints', function() {
 
         return supertest(app)
           .patch(`/api/users/${idToUpdate}`)
+          .set('Authorization', makeAuthHeader(testUsers[idToUpdate - 1]))
           .send({
             ...updateUser,
             fieldToIgnore: 'should not be in GET response'
