0f88e4c5545cd6b9567b5541cbcfdd17c2270a7a
David B <d.jbradley.ii@gmail.com>
2019-12-16 23:46:04 -0800

    Updates truncate call and moves user and event creation to separate file

--- test/events-service.spec.js
+++ test/events-service.spec.js
@@ -1,92 +1,13 @@
 const knex = require('knex');
 const EventsService = require('../src/events/events-service');
+const { makeUsersArray } = require('./users.fixtures');
+const { makeEventsArray } = require('./events.fixtures');
 
 describe(`Events service object`, function() {
     let db;
 
-    let testUsers = [
-        {
-            id: 1,
-            fname : "Rick",
-            lname : "Mcqueeney",
-            dob : new Date('10/31/1983'),
-            email : "rmcqueeney@gmail.com",
-            password : "password1",
-            marital_status : "Married",
-            occupation : "Marketing",
-            gender : "Male",
-            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
-        },
-        {
-            id: 2,
-            fname : "Summer",
-            lname : "Lane",
-            dob : new Date('02/02/1972'),
-            email : "slane@gmail.com",
-            password : "password2",
-            marital_status : "Married",
-            occupation : "Fashion Designer",
-            gender : "Female",
-            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
-        },
-        {
-            id: 3,
-            fname : "Larry",
-            lname : "Savage",
-            dob : new Date('06/10/1975'),
-            email : "lsavage@aol.com",
-            password : "password3",
-            marital_status : "Widow",
-            occupation : "Construction",
-            gender : "Male",
-            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
-        },
-    ];
-
-    let testEvents = [
-        {
-            id: 1,
-            organizer : 1,
-            title : "Event 1 Title",
-            event_purpose : "Singles Night",
-            restaurant : "Sonora Town",
-            address : "321 4th St, Los Angeles, CA 90003",
-            date : new Date('10/31/2020'),
-            time : "06:00 PM",
-            description : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.",
-            singles_only : false, 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
-        },
-        {
-            id: 2,
-            organizer : 3,
-            title : "Event 2 Title",
-            event_purpose : "Game Night",
-            restaurant : "Button Mash",
-            address : "123 2nd St, Los Angeles, CA 90001",
-            date : new Date('04/03/2020'),
-            time : "06:00 PM",
-            description : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.",
-            singles_only : false, 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
-        },
-        {
-            id: 3,
-            organizer : 1,
-            title : "Event 3 Title",
-            event_purpose : "Networking",
-            restaurant : "Water Grill",
-            address : "30923 Union Ave, Los Angeles, CA 90301",
-            date : new Date('03/03/2020'),
-            time : "06:00 PM",
-            description : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.",
-            singles_only : false, 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
-        },
-    ];
+    let testUsers = makeUsersArray();
+    let testEvents = makeEventsArray();
 
     before(() => {
         db = knex({
@@ -95,23 +16,27 @@ describe(`Events service object`, function() {
         })
     });
 
-    before(() => db('users').truncate());
+    before(() => {
+        db('events').truncate();
+        db('users').truncate();
+    });
 
-    afterEach(() => db('users').truncate());
+    afterEach(() => {
+        db('events').truncate();
+        db('users').truncate();
+    });
 
     after(() => db.destroy());
 
     context(`Given 'events' has data`, () => {
-        beforeEach(() => {
-            return db
-                .into('users')
-                .insert(testUsers)
+        afterEach(() => {
+            db('events').truncate();
+            db('users').truncate();
         });
-
+        
         beforeEach(() => {
-            return db
-                .into('events')
-                .insert(testEvents)
+            db('users').insert(testUsers);
+            db('events').insert(testEvents);
         });
 
         it(`getAllEvents() resolves all events from 'events' table`, () => {
@@ -164,7 +89,7 @@ describe(`Events service object`, function() {
                 restaurant: "Updated Restaurant",
                 address : "123 Update St, Norwalk CA 91919",
                 date : new Date('09/23/2020'),
-                time : "04:05 PM",
+                time : "16:05 PM",
                 description : "Updated Description",
                 singles_only : false
             };
@@ -181,16 +106,20 @@ describe(`Events service object`, function() {
     })
 
     context(`Given 'events' table has no data`, () => {
-        beforeEach(() => db('events').truncate());
+        beforeEach(() => {
+            db('events').truncate();
+            db('users').truncate();
+        });
 
         it(`insertEvents() inserts a new event and resolves the new event with an 'id'`, () => {
             const newEvent = {
+                organizer: 1,
                 title: "New Event Title",
                 event_purpose: "Social",
                 restaurant: "New Restaurant",
                 address : "123 New St, Norwalk CA 91919",
                 date : new Date('09/23/2020'),
-                time : "04:45 PM",
+                time : "16:45 PM",
                 description : "New Description",
                 singles_only : false
             }
