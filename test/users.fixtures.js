2c0c2698b11061801d0201566f36c3ed8cd5560e
David B <d.jbradley.ii@gmail.com>
2019-12-17 03:34:35 -0800

    Adds makeAuthUsersArray attempting to get tests to pass

--- test/users.fixtures.js
+++ test/users.fixtures.js
@@ -39,6 +39,51 @@ function makeUsersArray(){
     ];
 }
 
+function makeAuthUsersArray(){
+    return [
+        {
+            id: "1",
+            fname : "Rick",
+            lname : "Mcqueeney",
+            dob : '10/10/1980',
+            email : "rmcqueeney@gmail.com",
+            password : "Password1!",
+            marital_status : "Married",
+            occupation : "Marketing",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: '10/10/1980'
+        },
+        {
+            id: "2",
+            fname : "Summer",
+            lname : "Lane",
+            dob : '10/10/1980',
+            email : "slane@gmail.com",
+            password : "Password2!",
+            marital_status : "Married",
+            occupation : "Fashion Designer",
+            gender : "Female",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: '10/10/1980'
+        },
+        {
+            id: "3",
+            fname : "Larry",
+            lname : "Savage",
+            dob : '10/10/1980',
+            email : "lsavage@aol.com",
+            password : "Password3!",
+            marital_status : "Widow",
+            occupation : "Construction",
+            gender : "Male",
+            bio : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.", 
+            date_created: '10/10/1980'
+        },
+    ];
+}
+
 module.exports = {
     makeUsersArray,
+    makeAuthUsersArray
 }
