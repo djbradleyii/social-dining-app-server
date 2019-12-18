4e74afd9c9b38b6e66b04db9edf117b7ae32fc18
David B <d.jbradley.ii@gmail.com>
2019-12-18 15:26:27 -0800

    Adds attendees fixtures for attendees tests

---
+++ test/attendees.fixtures.js
@@ -0,0 +1,40 @@
+function makeAttendeesArray(){
+    return [
+        {
+            user_id : 1,
+            event_id : 1,
+        },
+        {
+            user_id : 1,
+            event_id : 3,
+        },
+        {
+            user_id : 1,
+            event_id : 2,
+        },
+        {
+            user_id : 3,
+            event_id : 1,
+        },
+        {
+            user_id : 3,
+            event_id : 3,
+        },
+        {
+            user_id : 3,
+            event_id : 2,
+        },
+        {
+            user_id : 2,
+            event_id : 3,
+        },
+        {
+            user_id : 2,
+            event_id : 2,
+        },
+    ];
+}
+
+module.exports = {
+    makeAttendeesArray,
+}
