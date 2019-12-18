8601ddbc8ff72820f81a2bf16f35477027e50996
David B <d.jbradley.ii@gmail.com>
2019-12-18 15:22:17 -0800

    Updates the dates used for the seeds

--- test/events.fixtures.js
+++ test/events.fixtures.js
@@ -6,11 +6,11 @@ function makeEventsArray(){
             event_purpose : "Singles Night",
             restaurant : "Sonora Town",
             address : "321 4th St, Los Angeles, CA 90003",
-            date : new Date('10/31/2020'),
+            date : new Date('10/31/1983'),
             time : "18:00:00",
             description : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.",
             singles_only : false, 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
+            date_created: new Date('10/31/2020')
         },
         {
             organizer : 3,
@@ -18,11 +18,11 @@ function makeEventsArray(){
             event_purpose : "Game Night",
             restaurant : "Button Mash",
             address : "123 2nd St, Los Angeles, CA 90001",
-            date : new Date('04/03/2020'),
+            date : new Date('10/31/1983'),
             time : "18:00:00",
             description : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.",
             singles_only : false, 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
+            date_created: new Date('10/31/2020')
         },
         {
             organizer : 1,
@@ -30,11 +30,11 @@ function makeEventsArray(){
             event_purpose : "Networking",
             restaurant : "Water Grill",
             address : "30923 Union Ave, Los Angeles, CA 90301",
-            date : new Date('03/03/2020'),
+            date : new Date('10/31/1983'),
             time : "18:00:00",
             description : "Nam ullamcorper finibus purus, id facilisis nisi scelerisque in. Aliquam vel nisi id tellus efficitur sagittis. Sed vel maximus erat. Nunc dapibus purus massa, in molestie ipsum gravida vel. Phasellus varius nec risus a ornare.",
             singles_only : false, 
-            date_created: new Date('2019-12-11T16:28:32.615Z')
+            date_created: new Date('10/31/2020')
         },
     ];
 }
