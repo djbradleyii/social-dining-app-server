2507f089b4794ec25955aa4f159f1b4e0c5e0cb8
David B <d.jbradley.ii@gmail.com>
2019-12-18 15:15:48 -0800

    Updates tests to attempt to fix them - incomplete

--- test/events-service.spec.js
+++ test/events-service.spec.js
@@ -3,7 +3,7 @@ const EventsService = require('../src/events/events-service');
 const { makeUsersArray } = require('./users.fixtures');
 const { makeEventsArray } = require('./events.fixtures');
 
-describe.skip(`Events service object`, function() {
+describe(`Events service object`, function() {
     let db;
 
     let testUsers = makeUsersArray();
@@ -16,59 +16,53 @@ describe.skip(`Events service object`, function() {
         })
     });
 
-    before(() => {
-        db('events').truncate();
-        db('users').truncate();
-    });
+    before(() => db.raw('TRUNCATE attendees, events, users RESTART IDENTITY CASCADE'));
 
-    afterEach(() => {
-        db('events').truncate();
-        db('users').truncate();
-    });
+    afterEach(() => db.raw('TRUNCATE attendees, events, users RESTART IDENTITY CASCADE'));
 
     after(() => db.destroy());
 
     context(`Given 'events' has data`, () => {
-        afterEach(() => {
-            db('events').truncate();
-            db('users').truncate();
-        });
-        
+        beforeEach(() => db.raw('TRUNCATE attendees, events, users RESTART IDENTITY CASCADE'));
+
         beforeEach(() => {
-            db('users').insert(testUsers);
-            db('events').insert(testEvents);
+            return db
+                .into('users')
+                .insert(testUsers)
+                .then(() => {
+                return db
+                    .into('events')
+                    .insert(testEvents)
+                })
         });
 
         it(`getAllEvents() resolves all events from 'events' table`, () => {
             return EventsService.getAllEvents(db)
-                .then(actual => {
-                    expect(actual).to.eql(testEvents.map(event => ({
-                               ...event,
-                               date: new Date(event.date),
-                               date_created: new Date(event.date_created)
-                    })))
-                })
+                .then(res => {
+                    expect(res).to.have.lengthOf(3)
+                    expect(res[0].organizer).to.eql(testEvents[0].organizer)
+                    expect(res[0].title).to.eql(testEvents[0].title)
+                    expect(res[0].event_purpose).to.eql(testEvents[0].event_purpose)
+                    expect(res[0].restaurant).to.eql(testEvents[0].restaurant)
+                    expect(res[0].address).to.eql(testEvents[0].address)
+                    expect(res[0].description).to.eql(testEvents[0].description)
+                    expect(res[0].singles_only).to.eql(testEvents[0].singles_only)
+                  })
         })
 
         it(`getEventByID() resolves an event by id from 'event' table`, () => {
             const thirdId = 3;
-            const thirdTestEvent = testEvent[thirdId - 1];
+            const thirdTestEvent = testEvents[thirdId - 1];
             return EventsService.getEventById(db, thirdId)
-                .then(actual => {
-                    expect(actual).to.eql({
-                        id: thirdId,
-                        organizer: thirdTestEvent.organizer,
-                        title: thirdTestEvent.title,
-                        event_purpose: ThirdTestEvent.event_purpose,
-                        restaurant: thirdTestEvent.restaurant,
-                        address: thirdTestEvent.address,
-                        date: new Date(thirdTestEvent.date),
-                        time: thirdTestEvent.time,
-                        description: thirdTestEvent.description,
-                        singles_only: thirdTestEvent.singles_only,
-                        date_created: new Date(thirdTestEvent.date_created)
-                    })
-                })
+                .then(res => {
+                    expect(res.organizer).to.eql(thirdTestEvent.organizer)
+                    expect(res.title).to.eql(thirdTestEvent.title)
+                    expect(res.event_purpose).to.eql(thirdTestEvent.event_purpose)
+                    expect(res.restaurant).to.eql(thirdTestEvent.restaurant)
+                    expect(res.address).to.eql(thirdTestEvent.address)
+                    expect(res.description).to.eql(thirdTestEvent.description)
+                    expect(res.singles_only).to.eql(thirdTestEvent.singles_only)
+                  })
         })
 
         it(`deleteEvent() removes a event by id from 'event' table`, () => {
@@ -77,7 +71,21 @@ describe.skip(`Events service object`, function() {
                 .then(() => EventsService.getAllEvents(db))
                 .then(allEvents => {
                     const expected = testEvents.filter(event => event.id !== eventId);
-                    expect(allEvents).to.eql(expected)
+                    expect(allEvents).to.have.lengthOf(2);
+                    expect(allEvents[0].organizer).to.eql(expected[0].organizer)
+                    expect(allEvents[0].title).to.eql(expected[0].title)
+                    expect(allEvents[0].event_purpose).to.eql(expected[0].event_purpose)
+                    expect(allEvents[0].restaurant).to.eql(expected[0].restaurant)
+                    expect(allEvents[0].address).to.eql(expected[0].address)
+                    expect(allEvents[0].description).to.eql(expected[0].description)
+                    expect(allEvents[0].singles_only).to.eql(expected[0].singles_only)
+                    expect(allEvents[1].organizer).to.eql(expected[1].organizer)
+                    expect(allEvents[1].title).to.eql(expected[1].title)
+                    expect(allEvents[1].event_purpose).to.eql(expected[1].event_purpose)
+                    expect(allEvents[1].restaurant).to.eql(expected[1].restaurant)
+                    expect(allEvents[1].address).to.eql(expected[1].address)
+                    expect(allEvents[1].description).to.eql(expected[1].description)
+                    expect(allEvents[1].singles_only).to.eql(expected[1].singles_only)
                 })
         })
 
@@ -89,28 +97,32 @@ describe.skip(`Events service object`, function() {
                 restaurant: "Updated Restaurant",
                 address : "123 Update St, Norwalk CA 91919",
                 date : new Date('09/23/2020'),
-                time : "16:05 PM",
+                time : "04:05 PM",
                 description : "Updated Description",
                 singles_only : false
             };
 
-            return EventsService.updateEventInfo(db, idOfEventToUpdate, eventUpdates)
+            return EventsService.updateEventById(db, idOfEventToUpdate, eventUpdates)
             .then(() => EventsService.getEventById(db, idOfEventToUpdate))
-            .then(event => {
-                expect(event).to.eql({
-                    id: idOfEventToUpdate,
-                    ...eventUpdates
-                })
-            })
+            .then(res => {
+                expect(res.organizer).to.eql(testEvents[idOfEventToUpdate - 1].organizer)
+                expect(res.title).to.eql(eventUpdates.title)
+                expect(res.event_purpose).to.eql(eventUpdates.event_purpose)
+                expect(res.restaurant).to.eql(eventUpdates.restaurant)
+                expect(res.address).to.eql(eventUpdates.address)
+                expect(res.description).to.eql(eventUpdates.description)
+                expect(res.singles_only).to.eql(eventUpdates.singles_only)
+              })
         })
     })
 
     context(`Given 'events' table has no data`, () => {
+        beforeEach(() => db.raw('TRUNCATE attendees, events RESTART IDENTITY CASCADE'));
         beforeEach(() => {
-            db('events').truncate();
-            db('users').truncate();
+            return db
+                .into('users')
+                .insert(testUsers)
         });
-
         it(`insertEvents() inserts a new event and resolves the new event with an 'id'`, () => {
             const newEvent = {
                 organizer: 1,
@@ -119,25 +131,20 @@ describe.skip(`Events service object`, function() {
                 restaurant: "New Restaurant",
                 address : "123 New St, Norwalk CA 91919",
                 date : new Date('09/23/2020'),
-                time : "16:45 PM",
+                time : "04:45 PM",
                 description : "New Description",
                 singles_only : false
             }
-
             return EventsService.insertEvent(db, newEvent)
-                .then(actual => {
-                    expect(actual).to.eql({
-                        id: 1,
-                        title: newEvent.title,
-                        event_purpose: newEvent.event_purpose,
-                        restaurant: newEvent.restaurant,
-                        address: newEvent.address,
-                        date: new Date(newEvent.date),
-                        time: newEvent.time,
-                        description: newEvent.description,
-                        singles_only: newEvent.singles_only
-                    })
-                })
+                .then(res => {
+                    expect(res.organizer).to.eql(newEvent.organizer)
+                    expect(res.title).to.eql(newEvent.title)
+                    expect(res.event_purpose).to.eql(newEvent.event_purpose)
+                    expect(res.restaurant).to.eql(newEvent.restaurant)
+                    expect(res.address).to.eql(newEvent.address)
+                    expect(res.description).to.eql(newEvent.description)
+                    expect(res.singles_only).to.eql(newEvent.singles_only)
+                  })
         })
 
     })
