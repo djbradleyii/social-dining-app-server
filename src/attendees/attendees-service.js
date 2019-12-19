8a5cde0fb82c4fc3a27449f3ab28c7492d7eb403
David B <d.jbradley.ii@gmail.com>
2019-12-19 03:07:24 -0800

    Adds necessary services for Attendees - fully tested

--- src/attendees/attendees-service.js
+++ src/attendees/attendees-service.js
@@ -3,6 +3,59 @@ const AttendeesService = {
         return knex.select('*')
             .from('attendees')
     },
+    getAllAttendeesNames(knex){
+        /* Get All Attendees with user names */
+        return knex.raw(`
+                select distinct users.fname as Attendee
+                from
+                attendees
+                join
+                users on users.id = attendees.user_id`)
+            .then(res => {
+                return res.rows
+            })
+    },
+    getAllAttendeesByEventId(knex, event_id){
+        /* Get all attendees for a specific event */
+        return knex.raw(`
+                    select users.id as user_id, events.id as event_id, events.title, users.fname as attendee
+                    from
+                    events
+                    inner join
+                    attendees
+                    on attendees.event_id = events.id
+                    inner join
+                    users
+                    on users.id = attendees.user_id
+                    where events.id = ${event_id}`)
+            .then(res => {
+                return res.rows
+            })
+    },
+    getAttendeesCountByEventId(knex, event_id){
+        return knex.raw(`
+                    select count(distinct attendees.user_id) as "Attendee_Count"
+                    from
+                    events
+                    inner join
+                    attendees
+                    on attendees.event_id = events.id
+                    inner join
+                    users
+                    on users.id = attendees.user_id
+                    where events.id = ${event_id}`)
+            .then(res => {
+                return res.rows
+            })
+    },
+    deleteAttendeeFromEvent(knex, user_id, event_id ){
+        return knex('attendees')
+        .where({
+            user_id: user_id,
+            event_id: event_id
+        })
+        .delete()
+    },
     insertAttendee(knex, newAttendee){
         return knex
             .insert(newAttendee)
@@ -12,24 +65,6 @@ const AttendeesService = {
                 return rows
             })
     },
-    getAllEventsByUserId(knex, user_id){
-        return knex
-            .from('attendees')
-            .select('*')
-            .where('user_id',user_id)
-    },
-    getAllAttendeesByEventId(knex, user_id, event_id){
-        return knex
-            .from('attendees')
-            .select('*')
-            .where('user_id',user_id).andWhere('event_id', event_id)
-            .first()
-    },
-    deleteAttendeeByAttendeeId(knex, attendee_id){
-        return knex('attendees')
-        .where('id', attendee_id)
-        .delete()
-    }
 };
 
 module.exports = AttendeesService;
