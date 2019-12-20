fa24281ac0a94a153d78fbbdc6dba3be80d23a76
David B <d.jbradley.ii@gmail.com>
2019-12-20 07:17:28 -0800

    adds curly braces around for look and if statement

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -9,16 +9,19 @@ authRouter
      const { email, password } = req.body;
      const loginUser = { email, password };
 
-    for (const [key, value] of Object.entries(loginUser))
-        if (value == null)
+    for (const [key, value] of Object.entries(loginUser)){
+        if (value == null){
             return res.status(400).json({
                 error: `Missing '${key}' in request body`
             })
+        }
+    }
 
     AuthService.getUserWithEmail(
         req.app.get('db'),
         loginUser.email
     )
+
     .then(dbUser => {
     if (!dbUser){
         return res.status(400).json({
