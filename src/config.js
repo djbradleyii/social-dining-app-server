55a33c316a19dba8965626761deb67babb5cb75d
David B <d.jbradley.ii@gmail.com>
2019-12-29 22:34:22 -0800

    Updates cors configuration

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:8000',
+    CLIENT_ORIGIN: 'https://infinite-eyrie-40606.herokuapp.com' || 'http://localhost:8000',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dbradley:password@localhost/social-dining-app',
     TEST_DATABASE_URL:'postgresql://dbradley:password@localhost/social-dining-app-test',
     JWT_SECRET: process.env.JWT_SECRET || 'superdupersecretword',
