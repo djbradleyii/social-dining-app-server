d5aa6b61192b00ee15b4925ecab01bc528865e0c
David B <d.jbradley.ii@gmail.com>
2020-01-01 05:11:52 -0800

    Updates cors CLIENT_ORIGIN

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: 'https://social-dining-app.now.sh/' || 'http://localhost:3000',
+    CLIENT_ORIGIN: 'https://social-dining-app.now.sh',
     //CLIENT_ORIGIN: 'http://localhost:3000',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dbradley:password@localhost/social-dining-app',
     TEST_DATABASE_URL:'postgresql://dbradley:password@localhost/social-dining-app-test',
