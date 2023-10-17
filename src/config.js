cd1a57eefb40cdaa89a786ec52a4db304f771ee7
David B <d.jbradley.ii@gmail.com>
2023-10-17 10:59:59 -0700

    Updates the endpoint

--- src/config.js
+++ src/config.js
@@ -1,10 +1,13 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: 'https://social-dining-app.now.sh',
+  NODE_ENV: process.env.NODE_ENV || "development",
+  CLIENT_ORIGIN: "https://social-dining-app-client.onrender.com",
   // CLIENT_ORIGIN: 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dbradley:password@localhost/social-dining-app',
-  TEST_DATABASE_URL: 'postgresql://dbradley:password@localhost/social-dining-app-test',
-  JWT_SECRET: process.env.JWT_SECRET || 'superdupersecretword',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '12h',
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://dbradley:password@localhost/social-dining-app",
+  TEST_DATABASE_URL:
+    "postgresql://dbradley:password@localhost/social-dining-app-test",
+  JWT_SECRET: process.env.JWT_SECRET || "superdupersecretword",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "12h",
 };
