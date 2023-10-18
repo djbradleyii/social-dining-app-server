b9457167ccc5c74bc4234d9346b9dfede843414b
David B <d.jbradley.ii@gmail.com>
2023-10-18 10:41:20 -0700

    Adds knex config and updates DB connection information

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+const { DATABASE_URL } = require("./config");
+
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: DATABASE_URL,
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
